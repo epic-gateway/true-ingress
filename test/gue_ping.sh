16b6b90873d9c119de9569a55332b37f8fb220b7
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-06-26 05:46:19 -0700

    adding scripts to create service forwarding to EGW

---
+++ test/gue_ping.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# syntax: $0 <node> <service-id> <remote-ip> <remote-port> <local-port> <delay>
+
+STEPS=4
+TUNNEL_PREFIX="10.2.1."
+
+NODE=$1
+SERVICE_ID=$2
+REMOTE_IP=$3
+REMOTE_PORT=$4
+LOCAL_PORT=$5
+DELAY=$6
+
+echo "PFC: TUNNEL PING: NODE='${NODE}' SERVICE_ID='${SERVICE_ID}' PROTO='${PROTO}' REMOTE_IP='${REMOTE_IP}' REMOTE_PORT='${REMOTE_PORT}' LOCAL_PORT='${LOCAL_PORT}' DELAY='${DELAY}'"
+
+echo "docker exec -itd ${NODE} bash -c nping -c0 --delay ${DELAY}s --udp --source-port ${LOCAL_PORT} --dest-port ${REMOTE_PORT} ${REMOTE_IP}"
+docker exec -itd ${NODE} bash -c "sleep 3 ; nping -c0 --delay ${DELAY}s --udp --source-port ${LOCAL_PORT} --dest-port ${REMOTE_PORT} ${REMOTE_IP}"
+
+echo -e "\n==============================================="
+echo "# TUNNEL PING: DONE"
---
+++ test/pfc_setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# syntax: $0 <node> <service-id> <proto> <service-ip> <service-port> <tunnel-ip> <tunnel-port> <foo-ip>
+
+# 1) setup GUE tunnel
+# 2) add routing to the tunnel
+# 3) send GUE ping
+
+STEPS=4
+TUNNEL_PREFIX="10.2.1."
+
+NODE=$1
+SERVICE_ID=$2
+PROTO=$3
+SERVICE_IP=$4
+SERVICE_PORT=$5
+# following information should be retrieved later from GUE ping
+TUNNEL_REMOTE_IP=$6
+#TUNNEL_REMOTE_PORT=${TUNNEL_PORT}
+TUNNEL_REMOTE_PORT=$7
+FOO_IP=$8
+
+TUNNEL_PORT=6080
+
+echo "PFC.ADD: NODE='${NODE}' SERVICE_ID='${SERVICE_ID}' PROTO='${PROTO}' SERVICE_IP='${SERVICE_IP}' SERVICE_PORT='${SERVICE_PORT}' TUNNEL_REMOTE_IP='${TUNNEL_REMOTE_IP}' TUNNEL_REMOTE_PORT='${TUNNEL_REMOTE_PORT}' FOO_IP='${FOO_IP}'"
+
+IFNAME="gue${SERVICE_ID}"
+CHECK=`docker exec -it ${NODE} bash -c "ip addr" | grep ${IFNAME}`
+if [ "${CHECK}" ] ; then
+    echo "Service ID ${SERVICE_ID} already exist"
+    exit 1
+fi
+
+# careful! works only with small numbers
+TUNNEL_LOCAL_IP=${TUNNEL_PREFIX}${SERVICE_ID}
+
+echo -e "\n==============================================="
+echo "# PFC.ADD: [1/${STEPS}] Create GUE tunnel (${IFNAME}) to ${TUNNEL_REMOTE_IP}:${TUNNEL_REMOTE_PORT})"
+
+docker exec -it ${NODE} bash -c "ip fou add port ${TUNNEL_PORT} gue"
+docker exec -it ${NODE} bash -c "ip link add name ${IFNAME} type ipip remote ${TUNNEL_REMOTE_IP} encap gue encap-sport ${TUNNEL_PORT} encap-dport ${TUNNEL_REMOTE_PORT}"
+
+CHECK=`docker exec -it ${NODE} bash -c "ip link" | grep ${IFNAME}`
+if [ ! "${CHECK}" ] ; then
+    echo "FAILED to create tunnel"
+    exit 1
+fi
+
+echo -e "\n==============================================="
+echo "# PFC.ADD: [2/${STEPS}] Assign IP ${TUNNEL_LOCAL_IP} to ${IFNAME} and bring it up"
+
+docker exec -it ${NODE} bash -c "ip addr add ${TUNNEL_LOCAL_IP}/24 dev ${IFNAME}"
+docker exec -it ${NODE} bash -c "ip link set ${IFNAME} up"
+# check
+docker exec -it ${NODE} bash -c "ip addr"
+
+echo -e "\n==============================================="
+echo "# PFC.ADD: [3/${STEPS}] Set Route for service ${FOO_IP}/32 via ${IFNAME}"
+
+docker exec -it ${NODE} bash -c "ip route add ${FOO_IP}/32 dev ${IFNAME}"
+# check
+docker exec -it ${NODE} bash -c "ip route"
+
+echo -e "\n==============================================="
+echo "# PFC.ADD: DONE"
