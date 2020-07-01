002b745a5f0768d6da9cf96b8300f25c222bd85e
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-07-01 03:55:16 -0700

    Adding simple tests with loading and unloading TC to EGW and NODE on demo topology

---
+++ test/ebpf/test_01.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Load eBPF on EGW, run ping 5.5.5.5 from Client and check tracefile.
+# usage: $0
+
+cd ..
+
+# setup topology
+./topo_setup.sh basic.cfg
+
+CLIENT="client"
+NODE="node1"
+PROXY="egw"
+PROXY_IP="5.5.5.5"
+
+echo "########################################################"
+echo "# Topology up'n'runnin. Hit <ENTER> to attach TC on EGW. #"
+echo "########################################################"
+
+read
+
+docker exec -it ${PROXY} bash -c "cd /tmp/.acnodal/bin ; ./attach_tc.sh eth1 egw"
+
+echo "#########################################"
+echo "# TC attached. Hit <ENTER> to run test. #"
+echo "#########################################"
+
+read
+
+# check traces before
+tail -n60 /sys/kernel/debug/tracing/trace
+
+# generate ICMP ECHO REQUEST + RESPONSE packets
+./test_icmp.sh ${PROXY_IP} "${CLIENT}"
+
+# check traces after
+tail -n60 /sys/kernel/debug/tracing/trace
+
+echo "#################################################"
+echo "# Test done. Hit <ENTER> to detach TC from EGW. #"
+echo "#################################################"
+
+read
+
+docker exec -it ${PROXY} bash -c "cd /tmp/.acnodal/bin ; ./detach_tc.sh eth1"
+
+echo "#################################################"
+echo "# TC detached. Hit <ENTER> to cleanup topology. #"
+echo "#################################################"
+
+read
+# cleanup topology
+./topo_cleanup.sh basic.cfg
