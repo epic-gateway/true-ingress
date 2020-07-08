61254892b8102b5a5a99f75fc1c63cf16f85ab6b
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-07-08 04:42:18 -0700

    including maps into TC

--- src/pfc_egress_tc.c
+++ src/pfc_egress_tc.c
@@ -15,16 +15,72 @@
 #include <linux/if_arp.h>
 
 #include "dump_tc.h"
-
-/* A minimal, stand-alone unit, which matches on all traffic
- * with the default classid (return code of -1) looks like: */
+#include "cfg_tc.h"
+#include "stat_tc.h"
+#include "maps_tc.h"
 
 //__section("egress")
 int pfc_tx(struct __sk_buff *skb)
 {
-    char msg[] = "PFC TX >> ifindex %u, len %u\n";
-    bpf_trace_printk(msg, sizeof(msg), skb->ifindex, skb->len);
+    char msg1[] = "PFC TX << ifindex %u, len %u\n";
+    bpf_trace_printk(msg1, sizeof(msg1), skb->ifindex, skb->len);
+
+    // get config
+    __u32 cfg_key = CFG_IDX_TX;
+    struct config *cfg = bpf_map_lookup_elem(&map_config, &cfg_key);
+    if (!cfg) {
+        char msg[] = "ERR: Config not found!\n";
+        bpf_trace_printk(msg, sizeof(msg));
+        return TC_ACT_UNSPEC;
+    }
+
+    char msg2[] = "CFG: id %u, flags %x, name %s\n";
+    bpf_trace_printk(msg2, sizeof(msg2), cfg->id, cfg->flags, cfg->name);
+
+    // start processing
+    stats_update(skb->ifindex, skb);
+    stats_print(skb->ifindex);
     dump_pkt(skb);
+    
+    struct endpoint ep = { 0 };
+
+    // check ROLE
+    if (cfg->flags & CFG_TX_PROXY) {
+        char msg3[] = "Is PROXY\n";
+        bpf_trace_printk(msg3, sizeof(msg3));
+
+        // get DEP
+        // ...
+        char msg4[] = "Parsed DEP: ip %x, port %u, proto %u\n";
+        bpf_trace_printk(msg4, sizeof(msg4), ep.ip, ep.port, ep.proto);
+        
+        // is Service endpoint?
+        struct service *svc = bpf_map_lookup_elem(&map_encap, &ep);
+        if (svc) {
+            char msg5[] = "GUE Encap\n";
+            bpf_trace_printk(msg5, sizeof(msg5));
+
+            return dump_action(TC_ACT_OK);
+        }
+    } else {
+        char msg3[] = "Is NODE\n";
+        bpf_trace_printk(msg3, sizeof(msg3));
+
+        // get SEP
+        // ...
+        char msg4[] = "Parsed SEP: ip %x, port %u, proto %u\n";
+        bpf_trace_printk(msg4, sizeof(msg4), ep.ip, ep.port, ep.proto);
+
+        // chack output mode
+        if (cfg->flags & CFG_TX_DSO) {
+            char msg6[] = "Output mode: DSO\n";
+            bpf_trace_printk(msg6, sizeof(msg6));
+        } else {
+            char msg6[] = "Output mode: Regular\n";
+            bpf_trace_printk(msg6, sizeof(msg6));
+        }
+    }
+
     return dump_action(TC_ACT_UNSPEC);
 }
 
--- src/pfc_ingress_tc.c
+++ src/pfc_ingress_tc.c
@@ -15,16 +15,70 @@
 #include <linux/if_arp.h>
 
 #include "dump_tc.h"
-
-/* A minimal, stand-alone unit, which matches on all traffic
- * with the default classid (return code of -1) looks like: */
+#include "cfg_tc.h"
+#include "stat_tc.h"
+#include "maps_tc.h"
 
 //__section("ingress")
 int pfc_rx(struct __sk_buff *skb)
 {
-    char msg[] = "PFC RX << ifindex %u, len %u\n";
-    bpf_trace_printk(msg, sizeof(msg), skb->ifindex, skb->len);
+    char msg1[] = "PFC RX << ifindex %u, len %u\n";
+    bpf_trace_printk(msg1, sizeof(msg1), skb->ifindex, skb->len);
+
+    // get config
+    __u32 cfg_key = CFG_IDX_RX;
+    struct config *cfg = bpf_map_lookup_elem(&map_config, &cfg_key);
+    if (!cfg) {
+        char msg[] = "ERR: Config not found!\n";
+        bpf_trace_printk(msg, sizeof(msg));
+        return TC_ACT_UNSPEC;
+    }
+
+    char msg2[] = "CFG: id %u, flags %x, name %s\n";
+    bpf_trace_printk(msg2, sizeof(msg2), cfg->id, cfg->flags, cfg->name);
+
+    // start processing
+    stats_update(skb->ifindex, skb);
+    stats_print(skb->ifindex);
     dump_pkt(skb);
+
+    // get DEP
+    struct endpoint ep = { 0 };
+    // ...
+    char msg4[] = "Parsed DEP: ip %x, port %u, proto %u\n";
+    bpf_trace_printk(msg4, sizeof(msg4), ep.ip, ep.port, ep.proto);
+    
+    // check packet for DECAP
+    if (cfg->flags & CFG_RX_GUE) {
+        char msg3[] = "Checking GUE\n";
+        bpf_trace_printk(msg3, sizeof(msg3));
+        
+        // is GUE endpoint?
+        __u32 *found = bpf_map_lookup_elem(&map_decap, &ep);
+        if (found) {
+            char msg5[] = "Processing GUE\n";
+            bpf_trace_printk(msg5, sizeof(msg5));
+            // control or data
+            // verify GUE header
+            // decap
+            return dump_action(TC_ACT_OK);
+        }
+    }
+
+    // check packet for DNAT
+    if (cfg->flags & CFG_RX_DNAT) {
+        char msg3[] = "Checking DNAT\n";
+        bpf_trace_printk(msg3, sizeof(msg3));
+        
+        // is PROXY endpoint?
+        struct endpoint *dnat = bpf_map_lookup_elem(&map_nat, &ep);
+        if (dnat) {
+            char msg5[] = "Processing DNAT\n";
+            bpf_trace_printk(msg5, sizeof(msg5));
+            return dump_action(TC_ACT_OK);
+        }
+    }
+
     return dump_action(TC_ACT_UNSPEC);
 }
 
