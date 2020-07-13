b232a75595b22f5a673e9ffda151cc72f0944e71
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-07-13 04:05:49 -0700

    adding factory functions for structures

--- src/struct_tc.h
+++ src/struct_tc.h
@@ -1,6 +1,8 @@
 #ifndef STRUCT_TC_H_
 #define STRUCT_TC_H_
 
+#include <bpf/bpf_endian.h>
+
 // #################
 // # TC STRUCTURES #
 // #################
@@ -13,6 +15,17 @@ struct endpoint {
     __u16  proto;                   /* IP proto */
 };
 
+struct endpoint *
+make_endpoint(struct endpoint *ref,
+              __u32  ip,
+              __u16  port,
+              __u16  proto)
+{
+    ref->ip      = bpf_htonl(ip);
+    ref->port    = bpf_htons(port);
+    ref->proto   = bpf_htons(proto);
+    return ref;
+}
 ////////////////////////////////
 // Tunnel
 struct tunnel {
@@ -22,16 +35,38 @@ struct tunnel {
     __u16  port_remote;           /* outer: target port */                // can be NAT, filled from GUE control packet
 };
 
+struct tunnel *
+make_tunnel(struct tunnel *ref,
+            __u32  ip_local,
+            __u16  port_local,
+            __u32  ip_remote,
+            __u16  port_remote)
+{
+    ref->ip_local      = bpf_htonl(ip_local);
+    ref->port_local    = bpf_htons(port_local);
+    ref->ip_remote     = bpf_htonl(ip_remote);
+    ref->port_remote   = bpf_htons(port_remote);
+    return ref;
+}
 ////////////////////////////////
 // Service identity
 
-#define GID_SID_TYPE        __u32
+#define GID_SID_TYPE        __u16
 
 struct identity {
     GID_SID_TYPE  service_id;              /* GUE service ID */
     GID_SID_TYPE  group_id;                /* GUE group ID */
 };
 
+struct identity *
+make_identity(struct identity *ref,
+              GID_SID_TYPE service_id,
+              GID_SID_TYPE group_id)
+{
+    ref->service_id = bpf_htons(service_id);
+    ref->group_id = bpf_htons(group_id);
+    return ref;
+}
 ////////////////////////////////
 // Security key
 
@@ -49,6 +84,17 @@ struct service {
     struct key      key;
 };
 
+struct service *
+make_service(struct service  *ref,
+             __u32            tunnel_id,
+             struct identity *identity,
+             struct key      *key)
+{
+    ref->tunnel_id  = bpf_htonl(tunnel_id);
+    ref->identity   = *identity;
+    ref->key        = *key;
+    return ref;
+}
 ////////////////////////////////
 // "Empty"
 //struct empty {
