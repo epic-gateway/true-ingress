920860483d90a4e921c20a0898fe1b9000dff7b6
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-07-15 07:28:05 -0700

    adding basic GUE control header parsing

--- src/pfc_ingress_tc.c
+++ src/pfc_ingress_tc.c
@@ -21,6 +21,21 @@
 
 #include "common_tc.h"
 
+//////////////////////////////
+struct guehdr {
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    __u8    hlen : 5,
+            control : 1,
+            version : 2;
+#else
+    __u8    version : 2,
+            control : 1,
+            hlen : 5;
+#endif
+    __u8 proto_ctype;
+    __u16 flags;
+};
+//////////////////////////////
 //__section("ingress")
 int pfc_rx(struct __sk_buff *skb)
 {
@@ -64,20 +79,69 @@ int pfc_rx(struct __sk_buff *skb)
         // is GUE endpoint?
         if (bpf_map_lookup_elem(&map_decap, &ep)) {
             bpf_print("Parsing GUE header\n");
+            void *data_end = (void *)(long)skb->data_end;
             // control or data
-            if (0) {
-                bpf_print("GUE Control: tunnel-id %u from %x:%u\n", 0, 0, 0);
+            struct guehdr *gue = hdr.payload;
+
+            if ((void*)&gue[1] > data_end)
+            {
+                bpf_print("ERROR: (GUE) Invalid packet size\n");
+                return dump_action(TC_ACT_SHOT);
+            }
+
+            bpf_print("  gue->version = %u\n", gue->version);
+            if (gue->version != 0) {
+                bpf_print("ERROR: Unsupported GUE version %u\n", gue->version);
+                return dump_action(TC_ACT_SHOT);
+            }
+
+            bpf_print("  gue->control = %u\n", gue->control);
+            if (gue->control) {
+                bpf_print("  gue->hlen = %u\n", gue->hlen);
+                if (gue->hlen != 1) {
+                    bpf_print("ERROR: Unexpected GUE control HLEN %u\n", gue->hlen);
+                    return dump_action(TC_ACT_SHOT);
+                }
+
+                __u32 *tunnel_id_ptr = (__u32 *)&gue[1];
+                if ((void*)&tunnel_id_ptr[1] > data_end)
+                {
+                    bpf_print("ERROR: (GUE) Invalid packet size\n");
+                    return dump_action(TC_ACT_SHOT);
+                }
+                __u32 tunnel_id = bpf_ntohl(*tunnel_id_ptr);
+                bpf_print("  tunnel_id = %u\n", tunnel_id);
+
+                // update map
+                struct tunnel *tun = bpf_map_lookup_elem(&map_tunnel, &tunnel_id);
+                if (!tun) {
+                    bpf_print("ERROR: Failed to update tunnel-id %u\n", tunnel_id);
+                    return dump_action(TC_ACT_SHOT);
+                }
+
+                parse_src_ep(&ep, &hdr);
+                bpf_print("GUE Control: Updating tunnel-id %u remote to %x:%u\n", tunnel_id, ep.ip, bpf_ntohs(ep.port));
+                tun->ip_remote = ep.ip;
+                tun->port_remote = ep.port;
+
+                return dump_action(TC_ACT_SHOT);
             } else {
+                if (gue->hlen != 5) {
+                    bpf_print("Unexpected GUE data HLEN %u\n", gue->hlen);
+                    return dump_action(TC_ACT_UNSPEC);
+                }
+
                 bpf_print("GUE Data: service-id %u, group-id %u\n", 0, 0);
                 if (0) {
-                    bpf_print("GUE service verification failed\n");
+                    bpf_print("ERROR: GUE service verification failed\n");
                     return dump_action(TC_ACT_SHOT);
                 }
 
                 bpf_print("GUE Decap\n");
                 // decap
+
+                return dump_action(TC_ACT_OK);
             }
-            return dump_action(TC_ACT_OK);
         }
     }
 
