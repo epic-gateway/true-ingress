39de46133048a3f02fb7c27598ed53f448ca0451
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-07-28 00:14:53 -0700

    addin option to send ping from guest, when no container name specified

--- test/test_icmp.sh
+++ test/test_icmp.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# usage: $0 <ip> "<node-list>"
-# example: $0 1.1.1.1 client1
-# example: $0 1.1.1.1 "client1 client2 client3"
+# usage: $0 <ip> ["<node-list>"]
+#
+# example: ping from container "client1"
+#   $0 1.1.1.1 client1
+# example: ping from multiple containers
+#   $0 1.1.1.1 "client1 client2 client3"
+# example: ping fron host
+#   $0 1.1.1.1
 
 if [ ! "$1" ] ; then
     echo "usage: $0 <ip> \"<node list>\""
@@ -9,12 +14,17 @@ if [ ! "$1" ] ; then
 fi
 
 IP=$1
-NODES=$2
+NODES="$2"
 
-for NODE in ${NODES}
-do
-    echo -e "\n# PING ${NODE} -> ${IP}"
-    docker exec -it ${NODE} bash -c "ping -q -c3 ${IP}"
-    #docker exec -it ${NODE} bash -c "nping -c3 --icmp ${IP}"
-done
+if [ "${NODES}" ] ; then
+    for NODE in ${NODES}
+    do
+        echo -e "\n# PING ${NODE} -> ${IP}"
+        docker exec -it ${NODE} bash -c "ping -q -c3 ${IP}"
+        #docker exec -it ${NODE} bash -c "nping -c3 --icmp ${IP}"
+    done
+else
+    ping -q -c3 ${IP}
+    #nping -c3 --icmp ${IP}
+fi
 
