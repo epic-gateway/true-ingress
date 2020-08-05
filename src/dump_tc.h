8fe7977c54157e9ff02a8d9226b53d47554a962a
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-08-05 05:16:27 -0700

    refactor: using bpf_print

--- src/dump_tc.h
+++ src/dump_tc.h
@@ -56,28 +56,23 @@ int dump_icmp(void *data, void *data_end)
 
     if (icmph->type == ICMP_ECHO)
     {
-        char msg2[] = "  ICMP ECHO REQUEST: id %u, seq %u\n";
-        bpf_trace_printk(msg2, sizeof(msg2), bpf_ntohs(icmph->un.echo.id), bpf_ntohs(icmph->un.echo.sequence));
+        bpf_print("  ICMP ECHO REQUEST: id %u, seq %u\n", bpf_ntohs(icmph->un.echo.id), bpf_ntohs(icmph->un.echo.sequence));
     }
     else if (icmph->type == ICMP_ECHOREPLY)
     {
-        char msg3[] = "  ICMP ECHO REPLY: id %u, seq %u\n";
-        bpf_trace_printk(msg3, sizeof(msg3), bpf_ntohs(icmph->un.echo.id), bpf_ntohs(icmph->un.echo.sequence));
+        bpf_print("  ICMP ECHO REPLY: id %u, seq %u\n", bpf_ntohs(icmph->un.echo.id), bpf_ntohs(icmph->un.echo.sequence));
     }
     else if (icmph->type == ICMP_TIME_EXCEEDED)
     {
-        char msg4[] = "  ICMP TIME EXCEEDED...\n";
-        bpf_trace_printk(msg4, sizeof(msg4));
+        bpf_print("  ICMP TIME EXCEEDED...\n");
     }
     else if (icmph->type == ICMP_DEST_UNREACH)
     {
-        char msg5[] = "  ICMP DEST UNREACHABLE...\n";
-        bpf_trace_printk(msg5, sizeof(msg5));
+        bpf_print("  ICMP DEST UNREACHABLE...\n");
     }
     else
     {
-        char msg6[] = "  ICMP : type %x, code %x\n";
-        bpf_trace_printk(msg6, sizeof(msg6), icmph->type, icmph->code);
+        bpf_print("  ICMP : type %x, code %x\n", icmph->type, icmph->code);
     }
 
     return TC_ACT_OK;
@@ -89,8 +84,7 @@ int dump_tcp(void *data, void *data_end)
     struct tcphdr *tcph = data;
     ASSERT((void*)&tcph[1] <= data_end, TC_ACT_SHOT, "ERROR: (TCP) Invalid packet size\n");
 
-    char msg2[] = "  TCP  : %d -> %d, csum 0x%x\n";
-    bpf_trace_printk(msg2, sizeof(msg2), bpf_ntohs(tcph->source), bpf_ntohs(tcph->dest), bpf_ntohs(tcph->check));
+    bpf_print("  TCP  : %d -> %d, csum 0x%x\n", bpf_ntohs(tcph->source), bpf_ntohs(tcph->dest), bpf_ntohs(tcph->check));
     /*char msg4[] = "       (";
     bpf_trace_printk(msg4, sizeof(msg4));
     if (tcph->rst)
@@ -125,8 +119,7 @@ int dump_udp(void *data, void *data_end)
     struct udphdr *udph = data;
     ASSERT((void*)&udph[1] <= data_end, TC_ACT_SHOT, "ERROR: (UDP) Invalid packet size\n");
 
-    char msg2[] = "  UDP  : %d -> %d, csum 0x%x\n";
-    bpf_trace_printk(msg2, sizeof(msg2), bpf_ntohs(udph->source), bpf_ntohs(udph->dest), bpf_ntohs(udph->check));
+    bpf_print("  UDP  : %d -> %d, csum 0x%x\n", bpf_ntohs(udph->source), bpf_ntohs(udph->dest), bpf_ntohs(udph->check));
 
     return TC_ACT_OK;
 }
@@ -137,10 +130,8 @@ int dump_ipv4(void *data, void *data_end)
     struct iphdr *iph = data;
     ASSERT((void*)&iph[1] <= data_end, TC_ACT_SHOT, "ERROR: (IPv4) Invalid packet size\n");
 
-    char msg2[] = "  IPv4 : %x -> %x, id %u\n";
-    bpf_trace_printk(msg2, sizeof(msg2), bpf_ntohl(iph->saddr), bpf_ntohl(iph->daddr), bpf_ntohs(iph->id));
-    char msg4[] = "    csum 0x%x\n";
-    bpf_trace_printk(msg4, sizeof(msg4), bpf_ntohs(iph->check));
+    bpf_print("  IPv4 : %x -> %x, id %u\n", bpf_ntohl(iph->saddr), bpf_ntohl(iph->daddr), bpf_ntohs(iph->id));
+    bpf_print("    csum 0x%x\n", bpf_ntohs(iph->check));
 
     if (iph->protocol == IPPROTO_ICMP)
         dump_icmp(&iph[1], data_end);
@@ -149,8 +140,7 @@ int dump_ipv4(void *data, void *data_end)
     else if (iph->protocol == IPPROTO_UDP)
         dump_udp(&iph[1], data_end);
     else {
-        char msg3[] = "    proto %u\n";
-        bpf_trace_printk(msg3, sizeof(msg3), iph->protocol);
+        bpf_print("    proto %u\n", iph->protocol);
     }
 
     return TC_ACT_OK;
@@ -159,8 +149,7 @@ int dump_ipv4(void *data, void *data_end)
 static inline
 int dump_ipv6(void *data, void *data_end)
 {
-    char msg[] = "  IPv6  : NOT SUPPORTED\n";
-    bpf_trace_printk(msg, sizeof(msg));
+    bpf_print("  IPv6  : NOT SUPPORTED\n");
 
     return TC_ACT_OK;
 }
@@ -173,8 +162,7 @@ int dump_vlan(void *data, void *data_end)
 
     __u16 h_proto = vhdr->h_vlan_encapsulated_proto;
 
-    char msg2[] = "  VLAN : id %u, proto %x\n";
-    bpf_trace_printk(msg2, sizeof(msg2), bpf_ntohs(vhdr->h_vlan_TCI), bpf_ntohs(h_proto));
+    bpf_print("  VLAN : id %u, proto %x\n", bpf_ntohs(vhdr->h_vlan_TCI), bpf_ntohs(h_proto));
 
     if (h_proto == bpf_htons(ETH_P_IP))
         dump_ipv4(&vhdr[1], data_end);
@@ -199,8 +187,7 @@ int dump_arp(void *data, void *data_end)
 
         __u32 *src = (__u32 *)arpexth->ar_sip;
         __u32 *dst = (__u32 *)arpexth->ar_tip;
-        char msg4[] = "  ARP WHO HAS: %x, tell %x\n";
-        bpf_trace_printk(msg4, sizeof(msg4), bpf_ntohl(*dst), bpf_ntohl(*src));
+        bpf_print("  ARP WHO HAS: %x, tell %x\n", bpf_ntohl(*dst), bpf_ntohl(*src));
     }
     else if (arph->ar_op == bpf_htons(ARPOP_REPLY))
     {
@@ -209,13 +196,11 @@ int dump_arp(void *data, void *data_end)
 
         __u32 *src = (__u32*)arpexth->ar_sip;
         __u32 *mac = (__u32*)&arpexth->ar_sha[2];
-        char msg5[] = "  ARP REPLY: %x -> %x\n";
-        bpf_trace_printk(msg5, sizeof(msg5), bpf_ntohl(*src), bpf_ntohl(*mac));
+        bpf_print("  ARP REPLY: %x -> %x\n", bpf_ntohl(*src), bpf_ntohl(*mac));
     }
     else
     {
-        char msg6[] = "  ARP  : op %u\n";
-        bpf_trace_printk(msg6, sizeof(msg6), bpf_ntohs(arph->ar_op));
+        bpf_print("  ARP  : op %u\n", bpf_ntohs(arph->ar_op));
     }
 
     return TC_ACT_OK;
@@ -228,13 +213,10 @@ int dump_eth(void *data, void *data_end)
     ASSERT((void*)&eth[1] <= data_end, TC_ACT_SHOT, "ERROR: (ETH) Invalid packet size\n");
 
     __u16 h_proto = eth->h_proto;
-
     __u32 *src = (__u32*)&eth->h_source[2];
     __u32 *dst = (__u32*)&eth->h_dest[2];
 
-    char msg2[] = "  ETH  : %x -> %x, proto %x\n";
-    bpf_trace_printk(msg2, sizeof(msg2), bpf_ntohl(*src), bpf_ntohl(*dst), bpf_ntohs(h_proto));
-//    bpf_trace_printk(msg2, sizeof(msg2), *src, *dst, bpf_ntohs(h_proto));
+    bpf_print("  ETH  : %x -> %x, proto %x\n", bpf_ntohl(*src), bpf_ntohl(*dst), bpf_ntohs(h_proto));
 
     if (h_proto == bpf_htons(ETH_P_IP))
         dump_ipv4(&eth[1], data_end);
@@ -251,17 +233,14 @@ int dump_eth(void *data, void *data_end)
 static inline
 int dump_pkt(struct __sk_buff *skb)
 {
-    char msg[] = "DUMP: =====================\n";
-    bpf_trace_printk(msg, sizeof(msg));
-    char msg1[] = "  Size : %u B\n";
-    bpf_trace_printk(msg1, sizeof(msg1), skb->len);
+    bpf_print("DUMP: =====================\n");
+    bpf_print("  Size : %u B\n", skb->len);
 
-    //dump_eth((void *)(long)skb->data, (void *)(long)skb->data_end);
     void *data = (void *)(long)skb->data;
-	void *data_end = (void *)(long)skb->data_end;
+    void *data_end = (void *)(long)skb->data_end;
     dump_eth(data, data_end);
 
-    bpf_trace_printk(msg, sizeof(msg));
+    bpf_print("DUMP: =====================\n");
 
     return TC_ACT_OK;
 }
@@ -273,44 +252,37 @@ int dump_action(int action) {
     {
         case TC_ACT_PIPE:
         {  
-            char msg1[] = "Action: TC_ACT_PIPE\n\n";
-            bpf_trace_printk(msg1, sizeof(msg1));
+            bpf_print("Action: TC_ACT_PIPE\n\n");
             break;
         }
         case TC_ACT_RECLASSIFY:
         {
-            char msg2[] = "Action: TC_ACT_RECLASSIFY\n\n";
-            bpf_trace_printk(msg2, sizeof(msg2));
+            bpf_print("Action: TC_ACT_RECLASSIFY\n\n");
             break;
         }
         case TC_ACT_OK:
         {
-            char msg3[] = "Action: TC_ACT_OK\n\n";
-            bpf_trace_printk(msg3, sizeof(msg3));
+            bpf_print("Action: TC_ACT_OK\n\n");
             break;
         }
         case TC_ACT_REDIRECT:
         {
-            char msg4[] = "Action: TC_ACT_REDIRECT\n\n";
-            bpf_trace_printk(msg4, sizeof(msg4));
+            bpf_print("Action: TC_ACT_REDIRECT\n\n");
             break;
         }
         case TC_ACT_SHOT:
         {
-            char msg5[] = "Action: TC_ACT_SHOT\n\n";
-            bpf_trace_printk(msg5, sizeof(msg5));
+            bpf_print("Action: TC_ACT_SHOT\n\n");
             break;
         }
         case TC_ACT_UNSPEC:
         {
-            char msg5[] = "Action: TC_ACT_UNSPEC\n\n";
-            bpf_trace_printk(msg5, sizeof(msg5));
+            bpf_print("Action: TC_ACT_UNSPEC\n\n");
             break;
         }
         default:
         {
-            char msg6[] = "Action: UNKNOWN (%d)\n\n";
-            bpf_trace_printk(msg6, sizeof(msg6));
+            bpf_print("Action: UNKNOWN (%d)\n\n");
             break;
         }
     }
