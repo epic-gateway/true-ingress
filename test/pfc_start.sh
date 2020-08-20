70feee8f83516300b7be37f47e511b11d7120512
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-08-20 03:19:41 -0700

    moving nohup to the beggining of the script to prevent random nohup failure

--- test/pfc_start.sh
+++ test/pfc_start.sh
@@ -24,6 +24,13 @@ if [ "${VERBOSE}" ]; then
     echo -e "\PFC.START : NIC='${NIC}' NAME='${NAME}' CONF_RX='${CONF_RX}' CONF_TX='${CONF_TX}' PORT_MIN='${PORT_MIN}' PORT_MAX='${PORT_MAX}' DELAY='${DELAY}'"
 fi
 
+if [ "${DELAY}" ]; then
+    #nohup bash -c "python3 /tmp/.acnodal/bin/gue_ping_svc_auto.py ${DELAY}" &
+    #nohup bash -c "python3 /tmp/.acnodal/bin/gue_ping_svc_auto.py ${DELAY} &> /tmp/gue_ping.log" &
+    nohup python3 /tmp/.acnodal/bin/gue_ping_svc_auto.py ${DELAY} &
+    #ps aux | grep "gue_ping"
+fi
+
 /tmp/.acnodal/bin/attach_tc.sh ${NIC}
 
 /tmp/.acnodal/bin/cli_cfg set ${NIC} 0 0 ${CONF_RX} "${NAME} RX"
@@ -35,12 +42,5 @@ if [ "${VERBOSE}" ]; then
 fi
 
 /tmp/.acnodal/bin/port_init.sh ${PORT_MIN} ${PORT_MAX}
-
-if [ "${DELAY}" ]; then
-    nohup bash -c "python3 /tmp/.acnodal/bin/gue_ping_svc_auto.py ${DELAY}" &
-    #nohup bash -c "python3 /tmp/.acnodal/bin/gue_ping_svc_auto.py ${DELAY} &> /tmp/gue_ping.log" &
-    #nohup python3 /tmp/.acnodal/bin/gue_ping_svc_auto.py ${DELAY} &
-    ps aux | grep "gue_ping"
-fi
 
 echo "# PFC.START : DONE"
