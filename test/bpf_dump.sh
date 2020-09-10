0e67ef3e07e6e0d718a0ed8d4f69661e008d5633
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-09-10 04:31:18 -0700

    adding helper to dump content of compiled bpf file

---
+++ test/bpf_dump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# usage: $0 <obj-file>
+#   <obj-file>  - path to bpf obj file
+# example:
+#    bpf_dump.sh pfc_ingress_tc.o
+#    bpf_dump.sh pfc_egress_tc.o
+
+if [ ! "$1" ]; then
+    echo "usage: $0 <obj-file>"
+    exit 1
+fi
+
+ls $1
+llvm-objdump -S --line-numbers -no-show-raw-insn $1 | less
+
