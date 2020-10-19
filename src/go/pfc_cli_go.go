e8f0ad1d671aa9e63f98ce7b3b53adc322d49cc1
toby cabot <toby@acnodal.io>
2020-10-19 17:40:07 -0400

    Run gofmt on our Go source code

--- src/go/pfc_cli_go.go
+++ src/go/pfc_cli_go.go
@@ -1,109 +1,110 @@
 package main
- 
+
 import (
-    "fmt"
-    "os"
-    "strconv"
-    "net"
-    "log"
-    "acnodal.io/pfc"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+
+	"acnodal.io/pfc"
 )
 
 func main() {
-    if (len(os.Args) < 2) {
-        fmt.Println(os.Args[0], "version")
-        fmt.Println(os.Args[0], "check")
-        fmt.Println(os.Args[0], "add <interface> <group-id> <service-id> <pwd> <proto> <proxy-ip> <proxy-port> <service-ip> <service-port> <gue-ip> <gue-port>")
-        fmt.Println(os.Args[0], "del <group-id> <service-id>")
-        fmt.Println(os.Args[0], "list")
-        fmt.Println(os.Args[0], "help")
-        return
-    }
+	if len(os.Args) < 2 {
+		fmt.Println(os.Args[0], "version")
+		fmt.Println(os.Args[0], "check")
+		fmt.Println(os.Args[0], "add <interface> <group-id> <service-id> <pwd> <proto> <proxy-ip> <proxy-port> <service-ip> <service-port> <gue-ip> <gue-port>")
+		fmt.Println(os.Args[0], "del <group-id> <service-id>")
+		fmt.Println(os.Args[0], "list")
+		fmt.Println(os.Args[0], "help")
+		return
+	}
 
-    switch os.Args[1] {
-        case "version":
-        fmt.Println(pfc.Version())
+	switch os.Args[1] {
+	case "version":
+		fmt.Println(pfc.Version())
 
-        case "check":
-        check, err := pfc.Check()
-        if check {
-            fmt.Println("PFC ready")
-        } else {
-            fmt.Println(err)
-        }
+	case "check":
+		check, err := pfc.Check()
+		if check {
+			fmt.Println("PFC ready")
+		} else {
+			fmt.Println(err)
+		}
 
-        case "add":
-        if (len(os.Args) < 11) {
-            fmt.Println(os.Args[0], " add <interface> <group-id> <service-id> <pwd> <proto> <proxy-ip> <proxy-port> <service-ip> <service-port> <gue-ip> <gue-port>")
-            return
-        }
+	case "add":
+		if len(os.Args) < 11 {
+			fmt.Println(os.Args[0], " add <interface> <group-id> <service-id> <pwd> <proto> <proxy-ip> <proxy-port> <service-ip> <service-port> <gue-ip> <gue-port>")
+			return
+		}
 
-        gid, _   := strconv.Atoi(os.Args[3])
-        sid, _   := strconv.Atoi(os.Args[4])
-        pip      := net.ParseIP(os.Args[7])
-        pport, _ := strconv.Atoi(os.Args[8])
-        sip      := net.ParseIP(os.Args[9])
-        sport, _ := strconv.Atoi(os.Args[10])
-        tip      := net.ParseIP(os.Args[11])
-        tport, _ := strconv.Atoi(os.Args[12])
+		gid, _ := strconv.Atoi(os.Args[3])
+		sid, _ := strconv.Atoi(os.Args[4])
+		pip := net.ParseIP(os.Args[7])
+		pport, _ := strconv.Atoi(os.Args[8])
+		sip := net.ParseIP(os.Args[9])
+		sport, _ := strconv.Atoi(os.Args[10])
+		tip := net.ParseIP(os.Args[11])
+		tport, _ := strconv.Atoi(os.Args[12])
 
-        ip, port, err := pfc.ForwardingAdd(os.Args[2], gid, sid, os.Args[5], os.Args[6], pip, pport, sip, sport, tip, tport)
-        if err != nil {
-            log.Println(err)
-        }
-        fmt.Printf("EGW GUE endpoint: %s:%d\n", ip, port)
+		ip, port, err := pfc.ForwardingAdd(os.Args[2], gid, sid, os.Args[5], os.Args[6], pip, pport, sip, sport, tip, tport)
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Printf("EGW GUE endpoint: %s:%d\n", ip, port)
 
-        case "del":
-        if (len(os.Args) < 4) {
-            fmt.Println(os.Args[0], " del <group-id> <service-id>")
-            return
-        }
+	case "del":
+		if len(os.Args) < 4 {
+			fmt.Println(os.Args[0], " del <group-id> <service-id>")
+			return
+		}
 
-        gid, _   := strconv.Atoi(os.Args[2])
-        sid, _   := strconv.Atoi(os.Args[3])
+		gid, _ := strconv.Atoi(os.Args[2])
+		sid, _ := strconv.Atoi(os.Args[3])
 
-        err := pfc.ForwardingRemove(gid, sid)
-        if err != nil {
-            log.Println(err)
-        }
+		err := pfc.ForwardingRemove(gid, sid)
+		if err != nil {
+			log.Println(err)
+		}
 
-        case "list":
-        t, s, err := pfc.ForwardingGetAll()
-        if err != nil {
-            log.Println(err)
-        }
-        fmt.Println("Tunnels")
-        for _, v := range t {
-            fmt.Printf("  id %d from (%s:%d) to (%s:%d)\n", v.Id, v.LocalIp, v.LocalPort, v.RemoteIp, v.RemotePort)
-        }
-        fmt.Println("")
-        fmt.Println("Services")
-        for _, v := range s {
-            fmt.Printf("  id (%d-%d) pwd '%s' Forwarding %s proxy %s:%d to %s:%d via tunnel %d\n", v.Gid, v.Sid, v.Pwd, v.Proto, v.ProxyIp, v.ProxyPort, v.BackendIp, v.BackendPort, v.Tid)
-        }
-        fmt.Println("")
+	case "list":
+		t, s, err := pfc.ForwardingGetAll()
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println("Tunnels")
+		for _, v := range t {
+			fmt.Printf("  id %d from (%s:%d) to (%s:%d)\n", v.Id, v.LocalIp, v.LocalPort, v.RemoteIp, v.RemotePort)
+		}
+		fmt.Println("")
+		fmt.Println("Services")
+		for _, v := range s {
+			fmt.Printf("  id (%d-%d) pwd '%s' Forwarding %s proxy %s:%d to %s:%d via tunnel %d\n", v.Gid, v.Sid, v.Pwd, v.Proto, v.ProxyIp, v.ProxyPort, v.BackendIp, v.BackendPort, v.Tid)
+		}
+		fmt.Println("")
 
-        tt, err1 := pfc.ForwardingGetTunnel(0)
-        if err1 {
-            fmt.Println("Tunnels")
-            fmt.Printf("  id %d from (%s:%d) to (%s:%d)\n", tt.Id, tt.LocalIp, tt.LocalPort, tt.RemoteIp, tt.RemotePort)
-        }
+		tt, err1 := pfc.ForwardingGetTunnel(0)
+		if err1 {
+			fmt.Println("Tunnels")
+			fmt.Printf("  id %d from (%s:%d) to (%s:%d)\n", tt.Id, tt.LocalIp, tt.LocalPort, tt.RemoteIp, tt.RemotePort)
+		}
 
-        tt, err1 = pfc.ForwardingGetTunnel(65636)
-        if ! err1 {
-            fmt.Println("Tunnels")
-            fmt.Printf("  id %d from (%s:%d) to (%s:%d)\n", tt.Id, tt.LocalIp, tt.LocalPort, tt.RemoteIp, tt.RemotePort)
-        }
+		tt, err1 = pfc.ForwardingGetTunnel(65636)
+		if !err1 {
+			fmt.Println("Tunnels")
+			fmt.Printf("  id %d from (%s:%d) to (%s:%d)\n", tt.Id, tt.LocalIp, tt.LocalPort, tt.RemoteIp, tt.RemotePort)
+		}
 
-        tt, err1 = pfc.ForwardingGetTunnel(65636)
-        if ! err1 {
-            fmt.Println("Tunnels")
-            fmt.Printf("  id %d from (%s:%d) to (%s:%d)\n", tt.Id, tt.LocalIp, tt.LocalPort, tt.RemoteIp, tt.RemotePort)
-        }
+		tt, err1 = pfc.ForwardingGetTunnel(65636)
+		if !err1 {
+			fmt.Println("Tunnels")
+			fmt.Printf("  id %d from (%s:%d) to (%s:%d)\n", tt.Id, tt.LocalIp, tt.LocalPort, tt.RemoteIp, tt.RemotePort)
+		}
 
-        case "help":
-        fmt.Println(os.Args[0], "version|add|del|list|help")
-        default:
-        fmt.Println("Unknown command '", os.Args[1], "'")
-    }
+	case "help":
+		fmt.Println(os.Args[0], "version|add|del|list|help")
+	default:
+		fmt.Println("Unknown command '", os.Args[1], "'")
+	}
 }
--- test/docker/gue_ping_svc_auto.go
+++ test/docker/gue_ping_svc_auto.go
@@ -1,206 +1,206 @@
 package main
- 
+
 import (
-    "fmt"
-    "os"
-    "time"
-    "strconv"
-    "net"
-    "os/exec"
-    "strings"
-    "bytes"
-    "encoding/binary"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func usage(name string) {
-    fmt.Printf("Usage: %s <ping-delay> <sweep-delay> <sweep-count>\n", name)
-    fmt.Printf("    <ping-delay> - delay between GUE control packets (in seconds)\n")
-    fmt.Printf("    <sweep-delay> - delay between session sweep checks (in seconds)\n")
-    fmt.Printf("    <sweep-count> - number of inactive intervals for session expiration\n")
-    fmt.Printf("\nExample : %s 10 10 60\n", name)
+	fmt.Printf("Usage: %s <ping-delay> <sweep-delay> <sweep-count>\n", name)
+	fmt.Printf("    <ping-delay> - delay between GUE control packets (in seconds)\n")
+	fmt.Printf("    <sweep-delay> - delay between session sweep checks (in seconds)\n")
+	fmt.Printf("    <sweep-count> - number of inactive intervals for session expiration\n")
+	fmt.Printf("\nExample : %s 10 10 60\n", name)
 }
 
 var (
-    tunnels      = map[int]int{}
-    session_hash = map[string]int{}
-    session_ttl  = map[string]int{}
-    sweep_counter int
+	tunnels       = map[int]int{}
+	session_hash  = map[string]int{}
+	session_ttl   = map[string]int{}
+	sweep_counter int
 )
 
 func send_ping(src_ip string, src_port string, dst_ip string, dst_port string, id int, pwd string) {
-    //fmt.Printf("%s:%s -> %s:%s (%d '%s')\n", src_ip, src_port, dst_ip, dst_port, id, pwd)
-    sport, _ := strconv.Atoi(src_port)
-    dport, _ := strconv.Atoi(dst_port)
+	//fmt.Printf("%s:%s -> %s:%s (%d '%s')\n", src_ip, src_port, dst_ip, dst_port, id, pwd)
+	sport, _ := strconv.Atoi(src_port)
+	dport, _ := strconv.Atoi(dst_port)
 
-    ServerAddr := net.UDPAddr{IP: net.ParseIP(dst_ip), Port: dport}
-    LocalAddr := net.UDPAddr{IP: net.ParseIP(src_ip), Port: sport}
+	ServerAddr := net.UDPAddr{IP: net.ParseIP(dst_ip), Port: dport}
+	LocalAddr := net.UDPAddr{IP: net.ParseIP(src_ip), Port: sport}
 
-    b := new(bytes.Buffer)
-    binary.Write(b, binary.BigEndian, uint32(0x2101a000))
-    binary.Write(b, binary.BigEndian, uint32(id))
-    binary.Write(b, binary.BigEndian, []byte(pwd))
+	b := new(bytes.Buffer)
+	binary.Write(b, binary.BigEndian, uint32(0x2101a000))
+	binary.Write(b, binary.BigEndian, uint32(id))
+	binary.Write(b, binary.BigEndian, []byte(pwd))
 
-    conn, err := net.DialUDP("udp", &LocalAddr, &ServerAddr)
-    if err  != nil {
-        fmt.Println("DialUDP: " , err)
-        return
-    }
-    
-    defer conn.Close()
+	conn, err := net.DialUDP("udp", &LocalAddr, &ServerAddr)
+	if err != nil {
+		fmt.Println("DialUDP: ", err)
+		return
+	}
 
-    _ ,err = conn.Write(b.Bytes())
-    if err != nil {
-        fmt.Println("Write: " , err)
-    }
+	defer conn.Close()
+
+	_, err = conn.Write(b.Bytes())
+	if err != nil {
+		fmt.Println("Write: ", err)
+	}
 }
 
 func tunnel_ping(timeout int) {
-    fmt.Println("Ping check")
+	fmt.Println("Ping check")
 
-    // set old tunnel list aside
-    tmp := map[int]int{}
-    for index, element := range tunnels {
-         tmp[index] = element
-    }
-    tunnels = map[int]int{}
+	// set old tunnel list aside
+	tmp := map[int]int{}
+	for index, element := range tunnels {
+		tmp[index] = element
+	}
+	tunnels = map[int]int{}
 
-    // get current tunnel list
-    cmd := "/tmp/.acnodal/bin/cli_service get all | grep 'VERIFY' | grep -v 'TABLE'"
-    out, err := exec.Command("bash", "-c", cmd).Output()
-    if err != nil {
-        fmt.Println("Error")
-        return
-    }
-    services := strings.Split(string(out), "\n")
+	// get current tunnel list
+	cmd := "/tmp/.acnodal/bin/cli_service get all | grep 'VERIFY' | grep -v 'TABLE'"
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		fmt.Println("Error")
+		return
+	}
+	services := strings.Split(string(out), "\n")
 
-    // read services for GUE HEADER info (group-id, service-id, key)"
-    verify := map[int]string{}
-    for _, service := range services[:len(services)-1] {
-        params := strings.Split(service, " ")
+	// read services for GUE HEADER info (group-id, service-id, key)"
+	verify := map[int]string{}
+	for _, service := range services[:len(services)-1] {
+		params := strings.Split(service, " ")
 
-        if params[0] != "VERIFY" {
-            continue
-        }
+		if params[0] != "VERIFY" {
+			continue
+		}
 
-        gid64, _ := strconv.ParseInt(strings.Split(params[8], "{")[1], 16, 32)
-        gid := int(gid64)
-        pwd := strings.Split(params[4], "'")[1]
-        verify[gid]=pwd
-    }
+		gid64, _ := strconv.ParseInt(strings.Split(params[8], "{")[1], 16, 32)
+		gid := int(gid64)
+		pwd := strings.Split(params[4], "'")[1]
+		verify[gid] = pwd
+	}
 
-    // read tunnels for outer header assembly
-    if len(verify) == 0 {
-        return
-    }
+	// read tunnels for outer header assembly
+	if len(verify) == 0 {
+		return
+	}
 
-    cmd = "/tmp/.acnodal/bin/cli_tunnel get all | grep 'TUN' | grep -v 'TABLE'"
-    out, err = exec.Command("bash", "-c", cmd).Output()
-    if err != nil {
-        fmt.Println("Error")
-        //log.Fatal(err1)
-        return
-    }
-    tnls := strings.Split(string(out), "\n")
+	cmd = "/tmp/.acnodal/bin/cli_tunnel get all | grep 'TUN' | grep -v 'TABLE'"
+	out, err = exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		fmt.Println("Error")
+		//log.Fatal(err1)
+		return
+	}
+	tnls := strings.Split(string(out), "\n")
 
-    for _, tunnel := range tnls[:len(tnls)-1] {
-        params := strings.Split(tunnel, "\t")
+	for _, tunnel := range tnls[:len(tnls)-1] {
+		params := strings.Split(tunnel, "\t")
 
-        if params[0] != "TUN" {
-            continue
-        }
+		if params[0] != "TUN" {
+			continue
+		}
 
-        tid, _ := strconv.Atoi(strings.Trim(params[1], " "))
-        ep := strings.Split(params[2], "->")
-        src := strings.Split(strings.Trim(ep[0], " "), ":")
-        dst := strings.Split(strings.Trim(ep[1], " "), ":")
+		tid, _ := strconv.Atoi(strings.Trim(params[1], " "))
+		ep := strings.Split(params[2], "->")
+		src := strings.Split(strings.Trim(ep[0], " "), ":")
+		dst := strings.Split(strings.Trim(ep[1], " "), ":")
 
-        t, ok := tmp[tid]
-        if ok && t < timeout {
-            tunnels[tid] = tmp[tid] + 1
-        } else {
-            fmt.Printf("  sending GUE ping %s:%s -> %s:%s (%d, '%s')\n", src[0], src[1], dst[0], dst[1], tid, verify[tid])
-            send_ping(src[0], src[1], dst[0], dst[1], tid, verify[tid])
+		t, ok := tmp[tid]
+		if ok && t < timeout {
+			tunnels[tid] = tmp[tid] + 1
+		} else {
+			fmt.Printf("  sending GUE ping %s:%s -> %s:%s (%d, '%s')\n", src[0], src[1], dst[0], dst[1], tid, verify[tid])
+			send_ping(src[0], src[1], dst[0], dst[1], tid, verify[tid])
 
-            tunnels[tid] = 1
-        }
-    }
-    fmt.Println(tunnels)
+			tunnels[tid] = 1
+		}
+	}
+	fmt.Println(tunnels)
 }
 
 func session_sweep(expire int) {
-    fmt.Println("Sweep check")
+	fmt.Println("Sweep check")
 
-    cmd := "/tmp/.acnodal/bin/cli_gc get all | grep 'ENCAP' | grep -v 'TABLE'"
-    out, err := exec.Command("bash", "-c", cmd).Output()
-    if err != nil {
-        fmt.Println("Error")
-        return
-    }
-    services := strings.Split(string(out), "\n")
-    
-    for _, service := range services[:len(services)-1] {
-        params := strings.Split(service, " ")
+	cmd := "/tmp/.acnodal/bin/cli_gc get all | grep 'ENCAP' | grep -v 'TABLE'"
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		fmt.Println("Error")
+		return
+	}
+	services := strings.Split(string(out), "\n")
 
-        if params[0] != "ENCAP" {
-            continue
-        }
+	for _, service := range services[:len(services)-1] {
+		params := strings.Split(service, " ")
 
-        key := strings.Split(service, "->")[0]
-        hash, _ := strconv.Atoi(strings.Split(params[6], "\t")[2])
+		if params[0] != "ENCAP" {
+			continue
+		}
 
-        if hash == 0 { // static record
-            continue
-        }
+		key := strings.Split(service, "->")[0]
+		hash, _ := strconv.Atoi(strings.Split(params[6], "\t")[2])
 
-        h, ok := session_hash[key]
-        if ok && h == hash {
-            if session_ttl[key] >= expire {
-                to_del := strings.Split(strings.Split(strings.Split(key, "(")[1], ")")[0], ",")
+		if hash == 0 { // static record
+			continue
+		}
 
-                fmt.Printf("  delete %s%s%s\n", to_del[0], to_del[1], to_del[2])
-                cmd := fmt.Sprintf("/tmp/.acnodal/bin/cli_gc del %s%s%s\n", to_del[0], to_del[1], to_del[2])
-                exec.Command("bash", "-c", cmd).Output()
+		h, ok := session_hash[key]
+		if ok && h == hash {
+			if session_ttl[key] >= expire {
+				to_del := strings.Split(strings.Split(strings.Split(key, "(")[1], ")")[0], ",")
 
-                delete(session_hash, key)
-                delete(session_ttl, key)
-            } else {
-                session_ttl[key] += 1
-            }
-        } else {
-            session_hash[key] = hash
-            session_ttl[key] = 1
-        }
-    }
+				fmt.Printf("  delete %s%s%s\n", to_del[0], to_del[1], to_del[2])
+				cmd := fmt.Sprintf("/tmp/.acnodal/bin/cli_gc del %s%s%s\n", to_del[0], to_del[1], to_del[2])
+				exec.Command("bash", "-c", cmd).Output()
+
+				delete(session_hash, key)
+				delete(session_ttl, key)
+			} else {
+				session_ttl[key] += 1
+			}
+		} else {
+			session_hash[key] = hash
+			session_ttl[key] = 1
+		}
+	}
 }
 
 func main() {
-    fmt.Println(os.Args[1:])
+	fmt.Println(os.Args[1:])
 
-    if (len(os.Args) < 4) {
-        usage(os.Args[0])
-        return
-    }
+	if len(os.Args) < 4 {
+		usage(os.Args[0])
+		return
+	}
 
-    tun_delay, _   := strconv.Atoi(os.Args[1])
-    sweep_delay, _ := strconv.Atoi(os.Args[2])
-    sweep_count, _ := strconv.Atoi(os.Args[3])
+	tun_delay, _ := strconv.Atoi(os.Args[1])
+	sweep_delay, _ := strconv.Atoi(os.Args[2])
+	sweep_count, _ := strconv.Atoi(os.Args[3])
 
-    counter := sweep_delay
+	counter := sweep_delay
 
-    fmt.Println("Starting PFC daemon")
+	fmt.Println("Starting PFC daemon")
 
-    for {
-        // GUE ping
-        tunnel_ping(tun_delay)
+	for {
+		// GUE ping
+		tunnel_ping(tun_delay)
 
-        // Session expiration
-        if counter < sweep_delay {
-            counter += 1
-        } else {
-            session_sweep(sweep_count)
-            counter = 1
-        }
+		// Session expiration
+		if counter < sweep_delay {
+			counter += 1
+		} else {
+			session_sweep(sweep_count)
+			counter = 1
+		}
 
-        time.Sleep(1 * time.Second)
-    }
+		time.Sleep(1 * time.Second)
+	}
 }
