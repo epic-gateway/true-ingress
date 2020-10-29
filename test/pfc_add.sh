8d63af7724f3ffaf3dca5556b5b1deb49c7af818
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-10-29 09:50:48 -0700

    updating original forwarding helper to handle iptional ifindex

--- test/pfc_add.sh
+++ test/pfc_add.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# syntax: $0 <nic> <group-id> <service-id> <passwd> <remote-tunnel-ip> <remote-tunnel-port> <proto> <proxy-ip> <proxy-port> <backend-ip> <backend-port>
+# syntax: $0 <nic> <group-id> <service-id> <passwd> <remote-tunnel-ip> <remote-tunnel-port> <proto> <proxy-ip> <proxy-port> <backend-ip> <backend-port> [<proxy-ifindex>]
 #       <nic>                   - Interface to sending one time GUE ping from
 #       <group-id>              - Group ID
 #       <service-id>            - Service ID
@@ -11,6 +11,7 @@
 #       <proxy-port>            - Service proxy port
 #       <backend-ip>            - Service backend IPv4 address
 #       <backend-port>          - Service backend port
+#       <proxy-ifindex>         - If-index of proxy instance container (optional)
 
 set -Eeo pipefail
 
@@ -28,6 +29,12 @@ PROXY_PORT=$9
 SERVICE_IP=${10}
 SERVICE_PORT=${11}
 
+if [ "${12}" ] ; then
+    PROXY_IFINDEX=${12}
+else
+    PROXY_IFINDEX=0
+fi
+
 TUNNEL_ID=${GROUP_ID}
 ((TUNNEL_ID <<= 16))
 ((TUNNEL_ID += ${SERVICE_ID}))
@@ -39,7 +46,7 @@ if [ ! "${LOCAL_TUN_PORT}" ] ; then
 fi
 
 if [ "${VERBOSE}" ]; then
-    echo -e "\nPFC.ADD : NIC='${NIC}' GROUP_ID='${GROUP_ID}' SERVICE_ID='${SERVICE_ID}' PASSWD='${PASSWD}' REMOTE_TUN_IP='${REMOTE_TUN_IP}' REMOTE_TUN_PORT='${REMOTE_TUN_PORT}' PROTO='${PROTO}' PROXY_IP='${PROXY_IP}' PROXY_PORT='${PROXY_PORT}' SERVICE_IP='${SERVICE_IP}' SERVICE_PORT='${SERVICE_PORT}'"
+    echo -e "\nPFC.ADD : NIC='${NIC}' GROUP_ID='${GROUP_ID}' SERVICE_ID='${SERVICE_ID}' PASSWD='${PASSWD}' REMOTE_TUN_IP='${REMOTE_TUN_IP}' REMOTE_TUN_PORT='${REMOTE_TUN_PORT}' PROTO='${PROTO}' PROXY_IP='${PROXY_IP}' PROXY_PORT='${PROXY_PORT}' SERVICE_IP='${SERVICE_IP}' SERVICE_PORT='${SERVICE_PORT}' PROXY_IFINDEX='${PROXY_IFINDEX}'"
     echo "    Local IP : ${LOCAL_TUN_IP}"
     echo "    Allocated tunnel port : ${LOCAL_TUN_PORT}"
     echo "    Tunnel-ID : ${TUNNEL_ID}"
@@ -51,7 +58,7 @@ cli_tunnel set ${TUNNEL_ID} ${LOCAL_TUN_IP} ${LOCAL_TUN_PORT} ${REMOTE_TUN_IP} $
 
 ## Setup service forwarding
 # cli_service set  <group-id>  <service-id>  <proto>  <ip-proxy>  <port-proxy>  <ip-ep>       <port-ep>       <tunnel-id> <key>
-cli_service set ${GROUP_ID} ${SERVICE_ID} ${PROTO} ${PROXY_IP} ${PROXY_PORT} ${SERVICE_IP} ${SERVICE_PORT} ${TUNNEL_ID} ${PASSWD}
+cli_service set ${GROUP_ID} ${SERVICE_ID} ${PROTO} ${PROXY_IP} ${PROXY_PORT} ${SERVICE_IP} ${SERVICE_PORT} ${TUNNEL_ID} ${PASSWD} ${PROXY_IFINDEX}
 
 if [ "${VERBOSE}" ]; then
     echo "# PFC.ADD : DONE"
