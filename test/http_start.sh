95c0ad0ae7cb4297de6b94863549b4b31bf8d606
Marek Zavodsky <marek.zavodsky@gmail.com>
2020-10-29 04:24:27 -0700

    minor cleanup

--- test/http_start.sh
+++ test/http_start.sh
@@ -40,11 +40,7 @@ docker exec -it ${NODE} bash -c "dd if=/dev/zero of=${HOME}/data_1M.bin count=1 
 echo -e "\n==============================================="
 echo "# HTTP.START [3/${STEPS}] : Starting HTTP server ID ${SERVICE_ID} on ${IP}:${PORT}"
 # TODO: append PID=... to info file
-#docker exec -itd ${NODE} bash -c "python3 /tmp/server.py ${IP} ${PORT} ${HOME} > ${HOME}/log 2>&1"
 docker exec -itd ${NODE} bash -c "python3 server.py ${IP} ${PORT} ${HOME} &> ${HOME}/log"
-#docker exec -it ${NODE} bash -c "nohup python3 /tmp/server.py ${IP} ${PORT} ${HOME} &> ${HOME}/log & echo $!"
-#docker exec -it ${NODE} bash -c "nohup python3 /tmp/server.py ${IP} ${PORT} ${HOME} & echo $!"
-#docker exec -it ${NODE} bash -c "python /tmp/server.py ${IP} ${PORT} ${HOME}"
 
 echo -e "\n==============================================="
 echo "# HTTP.START [4/${STEPS}] : Wait about ${DELAY} seconds for server going up"
