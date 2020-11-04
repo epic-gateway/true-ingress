e00b01d2c3e69bf488453747d1a31e2989af8104
toby cabot <toby@acnodal.io>
2020-11-04 09:45:33 -0500

    Add a gitlab ci config
    I had to change linux-headers-$(uname -r) to linux-headers-virtual
    to build inside docker because the running kernel might not be the
    same as what ubuntu 20.04 supports.

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-# usage $0 [<project-name>]
-#       <project-name> - folder name, where to clone sources. If not specified, repository name will be used.
 
 PACKAGES="clang \
     llvm \
     gcc-multilib \
+    golang-go \
     build-essential \
     python3 \
     python3-pip \
-    linux-headers-$(uname -r) \
+    linux-headers-virtual \
     libelf-dev \
     zlib1g-dev \
     vagrant"
 
-#set -e
-
-echo "============================="
-echo "# Install minimal dependencies"
-echo "============================="
-sudo apt -y update
-sudo apt install -y ${PACKAGES}
-sudo snap install go --classic
-
-echo "# DONE"
+apt -y update
+DEBIAN_FRONTEND=noninteractive apt install --yes ${PACKAGES}
