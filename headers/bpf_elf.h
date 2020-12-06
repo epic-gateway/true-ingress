d33617729a3ac7cecd109b5d76687f5c43609571
toby cabot <toby@acnodal.io>
2020-12-06 00:50:46 -0500

    Refresh bpf_elf.h from iproute2
    The motivation for this change was us wanting to add BTF metadata to
    our maps, and to do that we need the BPF_ANNOTATE_KV_PAIR macro. From
    what I can tell, the canonical version of bpf_elf.h is the one in the
    iproute2 project so I'll get it from there.
    
    I tried to update bpf_util.h, too, but that caused our compiles to
    fail so that will have to be a project for another day.

--- headers/bpf_elf.h
+++ headers/bpf_elf.h
@@ -1,27 +1,53 @@
-#ifndef ELF_MAP_H_
-#define ELF_MAP_H_
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __BPF_ELF__
+#define __BPF_ELF__
 
-/* Object pinning settings
- * A setting of PIN_GLOBAL_NS would place it into a global namespace,
- * so that it can be shared among different object files. A setting
- * of PIN_NONE (= 0) means no sharing, so each tc invocation a new map
- * instance is being created.
+#include <asm/types.h>
+
+/* Note:
+ *
+ * Below ELF section names and bpf_elf_map structure definition
+ * are not (!) kernel ABI. It's rather a "contract" between the
+ * application and the BPF loader in tc. For compatibility, the
+ * section names should stay as-is. Introduction of aliases, if
+ * needed, are a possibility, though.
  */
-#define PIN_NONE            0
-#define PIN_OBJECT_NS       1
-#define PIN_GLOBAL_NS       2
+
+/* ELF section names, etc */
+#define ELF_SECTION_LICENSE	"license"
+#define ELF_SECTION_MAPS	"maps"
+#define ELF_SECTION_PROG	"prog"
+#define ELF_SECTION_CLASSIFIER	"classifier"
+#define ELF_SECTION_ACTION	"action"
+
+#define ELF_MAX_MAPS		64
+#define ELF_MAX_LICENSE_LEN	128
+
+/* Object pinning settings */
+#define PIN_NONE		0
+#define PIN_OBJECT_NS		1
+#define PIN_GLOBAL_NS		2
 
 /* ELF map definition */
 struct bpf_elf_map {
-    __u32 type;
-    __u32 size_key;
-    __u32 size_value;
-    __u32 max_elem;
-    __u32 flags;
-    __u32 id;
-    __u32 pinning;
-    __u32 inner_id;
-    __u32 inner_idx;
+	__u32 type;
+	__u32 size_key;
+	__u32 size_value;
+	__u32 max_elem;
+	__u32 flags;
+	__u32 id;
+	__u32 pinning;
+	__u32 inner_id;
+	__u32 inner_idx;
 };
 
-#endif /* ELF_MAP_H_ */
+#define BPF_ANNOTATE_KV_PAIR(name, type_key, type_val)		\
+	struct ____btf_map_##name {				\
+		type_key key;					\
+		type_val value;					\
+	};							\
+	struct ____btf_map_##name				\
+	    __attribute__ ((section(".maps." #name), used))	\
+	    ____btf_map_##name = { }
+
+#endif /* __BPF_ELF__ */
