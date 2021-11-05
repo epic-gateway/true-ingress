82352a04f3f2e7d28d48455be244aa4880247188
toby cabot <toby@acnodal.io>
2021-11-05 13:42:08 -0400

    Move golang commands into a more golang-friendly directory structure
    As go compilers implement stronger error checking, things that used to
    be OK can become errors. In our case we had two programs in the same
    directory that both declared a "main" function. This started flagging
    errors with go 1.17:
    
    ../../../go/pkg/mod/gitlab.com/acnodal/epic/true-ingress/src/go@v0.0.0-20211105172050-ae069c7bbfd2/pfc_cli_go.go:16:6: main redeclared in this block
            /home/tobyc/go/pkg/mod/gitlab.com/acnodal/epic/true-ingress/src/go@v0.0.0-20211105172050-ae069c7bbfd2/gue_ping_svc_auto.go:148:6: previous declaration

--- src/go/pfc/pfc.go
+++ src/go/pfc/pfc.go
@@ -14,7 +14,7 @@ func Check() (bool, string) {
 	// see if we can run one of our executables
 	bytes, err := exec.Command("/opt/acnodal/bin/pfc_cli_go", "version").Output()
 	if err != nil {
-		return false, "PFC not compiled"
+		return false, "PFC not found"
 	}
 
 	return true, string(bytes)
