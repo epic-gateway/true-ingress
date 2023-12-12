00bd23f54c5727e0e225a380a4e90c3ebf360771
toby cabot <toby@acnodal.io>
2023-12-12 16:43:48 -0500

    Change from gid/sid to tunnelid
    Actually, we did this a while back when we started using the tunnel id
    in place of the gid/sid, but that meant that we sent two copies of the
    same thing in each packet. Now it's just the tunnel id.
    
    Signed-off-by: toby cabot <toby@acnodal.io>

--- cmd/gue_ping_svc_auto/main.go
+++ cmd/gue_ping_svc_auto/main.go
@@ -33,18 +33,11 @@ const (
 	GUEHeader uint32 = 0x2601a000
 )
 
-var (
-	tunnels = map[int]int{}
-)
-
 // sendPing sends an Acnodal EPIC GUE ping packet from localAddr to
-// remoteAddr. The packet contains the groupID, serviceID, tunnelID,
-// and pwd.
-func sendPing(localAddr net.UDPAddr, remoteAddr net.UDPAddr, groupID uint16, serviceID uint16, tunnelID uint32) error {
+// remoteAddr.
+func sendPing(localAddr net.UDPAddr, remoteAddr net.UDPAddr, tunnelID uint32) error {
 	b := new(bytes.Buffer)
 	binary.Write(b, binary.BigEndian, GUEHeader)
-	binary.Write(b, binary.BigEndian, groupID)
-	binary.Write(b, binary.BigEndian, serviceID)
 	binary.Write(b, binary.BigEndian, tunnelID)
 
 	conn, err := net.DialUDP("udp", &localAddr, &remoteAddr)
@@ -61,41 +54,10 @@ func sendPing(localAddr net.UDPAddr, remoteAddr net.UDPAddr, groupID uint16, ser
 	return err
 }
 
-func tunnelPing(timeout int) {
-	fmt.Println("Ping check")
-
-	// set old tunnel list aside
-	tmp := map[int]int{}
-	for index, element := range tunnels {
-		tmp[index] = element
-	}
-	tunnels = map[int]int{}
-
+func tunnelPing() {
 	// get current tunnel list
-	cmd := "/opt/acnodal/bin/cli_service get all | grep '^VERIFY'"
+	cmd := "/opt/acnodal/bin/cli_tunnel get all | grep '^TUN'"
 	out, err := exec.Command("bash", "-c", cmd).Output()
-	if err != nil {
-		fmt.Println(out)
-		fmt.Println(err)
-		return
-	}
-	services := strings.Split(string(out), "\n")
-
-	// read services for GUE HEADER info (group-id, service-id, key)"
-	gids := map[int]uint16{}
-	sids := map[int]uint16{}
-	for _, service := range services[:len(services)-1] {
-		params := strings.Split(service, " ")
-
-		g, _ := strconv.ParseInt(strings.Split(strings.Split(params[1], "(")[1], ",")[0], 10, 16)
-		s, _ := strconv.ParseInt(strings.Split(params[2], ")")[0], 10, 16)
-		tid, _ := strconv.ParseInt(strings.Split(params[3], "\t")[2], 10, 32)
-		gids[int(tid)] = uint16(g)
-		sids[int(tid)] = uint16(s)
-	}
-
-	cmd = "/opt/acnodal/bin/cli_tunnel get all | grep '^TUN'"
-	out, err = exec.Command("bash", "-c", cmd).Output()
 	if err != nil {
 		fmt.Println(out)
 		fmt.Println(err)
@@ -115,21 +77,14 @@ func tunnelPing(timeout int) {
 		src := strings.Split(strings.Trim(ep[0], " "), ":")
 		dst := strings.Split(strings.Trim(ep[1], " "), ":")
 
-		t, ok := tmp[tid]
-		if ok && t < timeout {
-			tunnels[tid] = tmp[tid] + 1
-		} else {
-			sPort, _ := strconv.Atoi(src[1])
-			dPort, _ := strconv.Atoi(dst[1])
+		sPort, _ := strconv.Atoi(src[1])
+		dPort, _ := strconv.Atoi(dst[1])
 
-			serverAddr := net.UDPAddr{IP: net.ParseIP(dst[0]), Port: dPort}
-			localAddr := net.UDPAddr{IP: net.ParseIP(src[0]), Port: sPort}
+		serverAddr := net.UDPAddr{IP: net.ParseIP(dst[0]), Port: dPort}
+		localAddr := net.UDPAddr{IP: net.ParseIP(src[0]), Port: sPort}
 
-			fmt.Printf("  sending GUE ping %s -> %s (%d)\n", localAddr.String(), serverAddr.String(), tid)
-			sendPing(localAddr, serverAddr, gids[tid], sids[tid], uint32(tid))
-
-			tunnels[tid] = 1
-		}
+		fmt.Printf("  sending GUE ping %s -> %s (%d)\n", localAddr.String(), serverAddr.String(), tid)
+		sendPing(localAddr, serverAddr, uint32(tid))
 	}
 }
 
@@ -144,7 +99,7 @@ func main() {
 	fmt.Println("Starting GUE ping daemon")
 
 	for {
-		tunnelPing(tun_delay)
-		time.Sleep(1 * time.Second)
+		tunnelPing()
+		time.Sleep(time.Duration(tun_delay) * time.Second)
 	}
 }
